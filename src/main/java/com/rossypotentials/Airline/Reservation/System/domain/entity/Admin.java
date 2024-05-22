cd9b75e90c1e0ed5376d126f2f92bbbb769a82fe
rossypotentials <urokorosemarynnena@gmail.com>
2024-05-22 10:10:24 +0100

    Added Role Based Access Control

--- src/main/java/com/rossypotentials/Airline/Reservation/System/domain/entity/Admin.java
+++ src/main/java/com/rossypotentials/Airline/Reservation/System/domain/entity/Admin.java
@@ -6,6 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,7 +19,40 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "Admin")
-public class Admin extends BaseClass{
-private String username;
+public class Admin extends BaseClass implements UserDetails {
+private String name;
+private String email;
+private String phoneNumber;
 private String password;
+private Boolean isEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ADMIN"));
+    }
+
+    @Override
+    public String getUsername() {
+    return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
 }
--- src/main/java/com/rossypotentials/Airline/Reservation/System/domain/entity/Passenger.java
+++ src/main/java/com/rossypotentials/Airline/Reservation/System/domain/entity/Passenger.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.yaml.snakeyaml.constructor.BaseConstructor;
 
 import java.util.Collection;
 import java.util.List;
